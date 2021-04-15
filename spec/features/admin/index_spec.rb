4faa46adde64c7695e7428d75c05224e1b7a19b8
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-14 23:46:33 -0700

    test

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe 'when I visit the admin dashboard' do
   before(:each) do
     @merchant_1 = FactoryBot.create(:merchant)
     @merchant_2 = FactoryBot.create(:merchant)
+    @cus1 = Customer.create!(first_name: 'Bob', last_name: 'Jordan')
+    @cus2 = Customer.create!(first_name: 'Pierce', last_name: 'Lamb')
+    @cus3 = Customer.create!(first_name: 'Stephen', last_name: 'Halkias')
+    @cus4 = Customer.create!(first_name: 'Nick', last_name: 'Mullen')
+    @cus5 = Customer.create!(first_name: 'Adam', last_name: 'Friedland')
+    @cus6 = Customer.create!(first_name: 'Gwion', last_name: 'Smith')
     @item_1 = @merchant_1.items.create!(name: 'Widget', description: 'it widgets', unit_price: 20)
     @item_2 = @merchant_1.items.create!(name: 'Thingamajig', description: 'useful for hair removal', unit_price: 4)
     @item_3 = @merchant_2.items.create!(name: 'Whatchamacallit', description: 'useful for growing hair', unit_price: 8)
     @item_4 = @merchant_2.items.create!(name: 'Macguffin', description: 'important for no particular reason', unit_price: 2)
+    
   end
 
   it 'displays header indicating that I am on the dashboard' do
