bb678bb7f29dacdd743565680563430eb6803c62
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-14 21:10:50 -0700

    began writing tests, stoppedd due to unfamiliarity with factory bot

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,5 @@
 class Admin::AdminController < ApplicationController
   def index
+    # @customers = Customer.top_five_customers
   end
 end
--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'when I visit the admin dashboard' do
+  before(:each) do
+    @merchant_1 = FactoryBot.create(:merchant)
+    @merchant_2 = FactoryBot.create(:merchant)
+    @item_1 = @merchant_1.items.create!(name: 'Widget', description: 'it widgets', unit_price: 20)
+    @item_2 = @merchant_1.items.create!(name: 'Thingamajig', description: 'useful for hair removal', unit_price: 4)
+    @item_3 = @merchant_2.items.create!(name: 'Whatchamacallit', description: 'useful for growing hair', unit_price: 8)
+    @item_4 = @merchant_2.items.create!(name: 'Macguffin', description: 'important for no particular reason', unit_price: 2)
+  end
+
   it 'displays header indicating that I am on the dashboard' do
     visit '/admin'
     expect(page).to have_content("Admin Dashboard")
@@ -14,5 +23,23 @@ RSpec.describe 'when I visit the admin dashboard' do
   it 'shows a link to the admin invoices index' do
     visit '/admin'
     expect(page).to have_link('Admin Invoices Index', href: admin_invoices_path)
+  end
+  
+  it "shows top 5 customers in the admin dashboard" do
+    expect(page).to have_content("Top 5 Customers")
+      within("#customer-#{@customer_number.id}") do
+        expect(page).to have_content(@customer_number.first_name)
+        expect(page).to have_content(@customer_number.first_name)
+        expect(page).to have_content(@customer_number.first_name)
+        expect(page).to have_content(@customer_number.last_name)
+        expect(page).to have_content(@customer_number.last_name)
+        expect(page).to have_content(@customer_number.last_name)
+      end
+    end
+  end
+
+  it "shows incomplete invoices" do
+    visit "/admin"
+    expect(page).to have_content("Incomplete Invoices")
   end
 end
